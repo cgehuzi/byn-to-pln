4b2b0b019e6c37850f972b717271f89c152853b2
cgehuzi <mail.cgehuzi@gmail.com>
2023-03-18 20:57:48 +0100

    Updates

---
+++ precache-manifest.cc229183add511ac6504a12b54fce5d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5306fcbb5c6ac0d3f09",
+    "url": "/byn-to-pln/static/css/main.213a1de9.chunk.css"
+  },
+  {
+    "revision": "a5306fcbb5c6ac0d3f09",
+    "url": "/byn-to-pln/static/js/main.a5306fcb.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/byn-to-pln/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/byn-to-pln/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "db96c72332c05e5ddc41",
+    "url": "/byn-to-pln/static/js/runtime~main.db96c723.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/byn-to-pln/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b4145d6597af74293619d4e52c5f0af9",
+    "url": "/byn-to-pln/index.html"
+  }
+];
